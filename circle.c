321ddeed92d9e78bc4fe8617fc3a6b101cb6f134
AmanPraharaj <99335765+AmanPraharaj@users.noreply.github.com>
2022-02-12 20:35:44 +0530

    Create circle.c

---
+++ circle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+float r,a,b,pi=3.14;
+printf("enter radius of circle");
+scanf("%f",&r);
+a=(pi*r*r);
+b=(2*pi*r);
+
+printf("area=%f",a);
+printf("perimeter is %f",b);
+
+
+
+
+
+}
