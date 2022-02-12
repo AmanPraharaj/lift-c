146522e934de6d049cafe338877409d5af039f26
AmanPraharaj <99335765+AmanPraharaj@users.noreply.github.com>
2022-02-12 20:34:01 +0530

    Create beginnerone.c

---
+++ beginnerone.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+printf ("name=aman praharaj\n");
+printf("course=btech\n");
+printf("stream=information technology\n");
+printf("my hobbies are playing call of duty mobile\n");
+
+
+
+
+}
